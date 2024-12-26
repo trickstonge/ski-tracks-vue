2d5f887efc00436a61ab568df8ac9d7987e3b1e0
Patrick <pstonge@overdriveinteractive.com>
2024-12-26 16:10:12 -0500

    init

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+        },
+        screens: {
+            '2xl': '1536px',
+            'xl': {'max': '1279px'},
+            'lg': {'max': '1023px'},
+            'md': {'max': '767px'},
+            'sm': {'max': '639px'},
+            }
+    },
+
+    plugins: [forms],
+};
